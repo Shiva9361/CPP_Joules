ef84637941405e3ffa2fecf695cb5cb61c58f8e9
Shiva9361 <shivadharshansankar936@gmail.com>
2024-10-05 16:50:37 +0530

    feat: installers

--- installer.bat
+++ installer.bat
@@ -1,9 +1,3 @@
-git clone https://github.com/Shiva9361/CPP_Joules.git
-mkdir include
-cd CPP_Joules
-mkdir build
-cd build
-cmake -G "MinGW Makefiles" ..
-make
-copy ..\include\* ..\..\include
-copy libCPP_Joules.dll ..\..\
+wget https://github.com/Shiva9361/CPP_Joules/releases/download/v1.0/CPP_Joules_windows.tar.gz
+tar -xvzf CPP_Joules_windows.tar.gz 
+rm CPP_Joules_windows.tar.gz
---
+++ installer.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Shiva9361/CPP_Joules/releases/download/v1.0/CPP_Joules_linux.tar.gz
+tar -xvzf CPP_Joules_linux.tar.gz 
+rm CPP_Joules_linux.tar.gz
