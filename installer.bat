731e141812ed4f2ac7b29b64e0777e7563c88540
Shiva9361 <shivadharshansankar936@gmail.com>
2024-10-07 19:11:17 +0530

    fix: windows installer

--- installer.bat
+++ installer.bat
@@ -1,3 +1,5 @@
-wget https://github.com/Shiva9361/CPP_Joules/releases/download/v1.0/CPP_Joules_windows.tar.gz
+curl -l0 https://github.com/Shiva9361/CPP_Joules/releases/download/v1.0/CPP_Joules_windows.tar.gz --output CPP_Joules_windows.tar.gz
 tar -xvzf CPP_Joules_windows.tar.gz 
-rm CPP_Joules_windows.tar.gz
+del CPP_Joules_windows.tar.gz
+@REM cd cppJoules
+@REM copy /s /h "libCPP_Joules.dll" "%windir%\system32\"
